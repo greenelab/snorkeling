e757e7a1eedee597d2269d3d2ac99767d55c6d36
danich1 <dnicholson329@gmail.com>
2017-01-06 11:13:41 -0500

    Adding the files for a test run

---
+++ set_env.sh
@@ -0,0 +1,7 @@
+export SNORKELHOME="$HOME/Documents/snorkel"
+echo "Snorkel home directory: $SNORKELHOME"
+export PYTHONPATH="$PYTHONPATH:$SNORKELHOME:$SNORKELHOME/treedlib"
+export PATH="$PATH:$SNORKELHOME:$SNORKELHOME/treedlib"
+export WORKINGPATH="$HOME/Documents/snorkeling"
+echo "$PYTHONPATH"
+echo "Environment variables set!"
