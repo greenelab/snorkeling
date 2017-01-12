ab157c173248b98f51bdcba768d4d0474cb10e6d
danich1 <dnicholson329@gmail.com>
2017-01-12 10:38:52 -0500

    More changes

--- run.sh
+++ run.sh
@@ -6,9 +6,6 @@ git submodule update --init --recursive
 #Change directory into snorkel
 cd "$SNORKELHOME"
 
-# Make sure the submodules are installed
-git submodule update --init --recursive
-
 # Make sure parser is installed
 PARSER="$SNORKELHOME/parser/stanford-corenlp-3.6.0.jar"
 if [ ! -f "$PARSER" ]; then
