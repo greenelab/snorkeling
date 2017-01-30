5137ba8b413ccb2dc2e496a6d540be6a7d899460
danich1 <dnicholson329@gmail.com>
2017-01-30 11:48:03 -0500

    Making necessary changes

--- Epilepsy/epilepsy_utils.py
+++ Epilepsy/epilepsy_utils.py
@@ -1,13 +1,23 @@
-import cPickle
-
 from collections import defaultdict
 from itertools import product
-from pandas import DataFrame
 from string import punctuation
 
-#Ripped off from the snorkel custom tagger.
-#Designed to get the offset where a given token is found so it can receive a custom entity tag (i.e. Gene or Chemical)
+from pandas import DataFrame
+
 def offsets_to_token(left, right, offset_array, lemmas, punc=set(punctuation)):
+    """Calculate the offset from tag to token
+
+    Ripped off from the snorkel custom tagger.
+    Designed to get the offset where a given token is found so it can receive a custom entity tag.
+    (i.e. Gene or Chemical)
+
+    Keyword arguments
+    left - the start of the tag
+    right - the end of the tag
+    offset_array - array of offsets given by stanford corenlp 
+    lemmas - array of lemmas given by stanford corenlp
+    punc - a list of punctuation characters
+    """
     token_start, token_end = None, None
     for i, c in enumerate(offset_array):
         if left >= c:
@@ -21,24 +31,71 @@ def offsets_to_token(left, right, offset_array, lemmas, punc=set(punctuation)):
 
 
 
-#This is a custom class that is designed to tag each relevant word in a given sentence
-# i.e if it see GAD then this tagger will give it a Gene tag.
 class Tagger(object):
+    """Custom Tagger Class
+    This is a custom class that is designed to tag each relevant word in a given sentence
+    i.e if it see GAD then this tagger will give GAD a Gene tag.
+    """
+    
     def __init__(self,file_name):
+        """ Initialize the tagger class
+
+        Keyword arguments:
+        self -- the class object
+        file_name -- the name of the file that contains the document annotations
+        """
         self.open_file = open(file_name,"r")
 
-    #doing this because not guarenteed to be feed a sorted document list
+    
+    def __del__(self):
+        self.open_file.close()
+
     def retrieve_document(self,pubmed_id):
+        """Retrieve pubtator's annotations and pythonize them.
+        
+        Keyword arguments:
+        self -- The class object
+        pubmed_id -- The id of the pubmed abstract
+
+        Returns:
+        A python object containing the annotations specific to a given document
+        """
+
+        #perform inline search to find correct document
         for line in self.open_file:
-            file_descriptor = line.split("::")
+            file_descriptor, tags = line.rstrip('\r\n').split("::")
             if file_descriptor[0] == pubmed_id:
-                document,tags = line.strip("\n").split("::")
                 break
-        self.open_file.seek(0)
-        return [tuple([x if "|" in x else int(x) for x in data.split(",")]) for data in tags.split(":") if len(data.split(",")) == 3]
+        #Set file pointer back to beginning of file 
+        self.open_file.seek(0) 
+        converted_tags = []
 
+        #for each document annotation pythonize it
+        for data in tags.split(":"):
+            tagged_offsets = data.split(",")
+            if len(tagged_offsets) == 3:
+                temp_tags = []
+                for x in data.split(","):
+                    if "|" in x:
+                        temp_tags.append(x)
+                    else:
+                        temp_tags.append(int(x))
+                converted_tags.append(tuple(temp_tags))
 
+        return converted_tags
+
+    
     def tag(self, parts):
+        """Tag each Sentence
+
+        Keyword arguments:
+        self -- the class object
+        parts -- standford's corenlp object which consists of nlp properties
+
+        Returns:
+        An updated parts object containing specified custom tags.
+        """
+
         pubmed_id, _, _, sent_start, sent_end = parts['stable_id'].split(':')
         sent_start, sent_end = int(sent_start), int(sent_end)
         tags = self.retrieve_document(pubmed_id)
