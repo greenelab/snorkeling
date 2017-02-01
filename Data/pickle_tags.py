926a6ccd0bfa984de72f07a6cdec0b7cdd909384
danich1 <dnicholson329@gmail.com>
2017-02-01 12:58:52 -0500

    pep8ified my code

--- Data/pickle_tags.py
+++ Data/pickle_tags.py
@@ -2,34 +2,35 @@ from lxml import etree as ET
 import tqdm
 import sys
 
-def convert(xml_file,output_file):
-	""" Extract the annotations from pubtator xml formatted file
-	
-	Keywords arguments:
-	xml_file -- The path to the xml data file
-	output_file -- the path to output the formatted data
-	"""
-	with open(output_file, "w") as g:
-		g.write("Document\tType\tID\tOffset\tEnd")
 
-		for event, document in tqdm.tqdm(ET.iterparse(xml_file, tag="document")):
-			pubmed_id = document[0].text
-			
-			#cycle through all the annotation tags contained within document tag
-			for annotation in document.iter('annotation'):
-				#not all annotations will contain an ID
-				if len(annotation) > 3:
-					ant_type = annotation[0]
-					ant_id = annotation[3]
-					offset = int(annotation[1].attrib['offset'])
-					end = offset + int(annotation[1].attrib['length'])
-					g.write("%s\t%s\t%s\t%s\n" % (pubmed_id, ant_type, ant_id, offset, end))
-					
-			#prevent memory overload
-			document.clear()
+def convert(xml_file, output_file):
+    """ Extract the annotations from pubtator xml formatted file
+
+    Keywords arguments:
+    xml_file -- The path to the xml data file
+    output_file -- the path to output the formatted data
+    """
+    with open(output_file, "w") as g:
+        g.write("Document\tType\tID\tOffset\tEnd")
+
+        for event, document in tqdm.tqdm(ET.iterparse(xml_file, tag="document")):
+            pubmed_id = document[0].text
+
+            # cycle through all the annotation tags contained within document tag
+            for annotation in document.iter('annotation'):
+                # not all annotations will contain an ID
+                if len(annotation) > 3:
+                    ant_type = annotation[0]
+                    ant_id = annotation[3]
+                    offset = int(annotation[1].attrib['offset'])
+                    end = offset + int(annotation[1].attrib['length'])
+                    g.write("%s\t%s\t%s\t%s\n" % (pubmed_id, ant_type, ant_id, offset, end))
+
+            # prevent memory overload
+            document.clear()
 
 
-#Main 
-xml_file = sys.argv[1] #path to file im using now /home/davidnicholson/Documents/snorkeling/data/Epilepsy/epilepsy_data.xml
-output_file = sys.argv[2] #name of file to write out
-extract_annotations(xml_file,output_file)
+# Main
+xml_file = sys.argv[1]  # path to file im using now /home/davidnicholson/Documents/snorkeling/data/Epilepsy/epilepsy_data.xml
+output_file = sys.argv[2]  # name of file to write out
+extract_annotations(xml_file, output_file)
--- Data/pubtator_to_xml.py
+++ Data/pubtator_to_xml.py
@@ -3,146 +3,150 @@ import gzip
 import tqdm
 
 
-def generate_annotation(file_object,tagged_term,index):
-	"""Generates the annotation tag
+def generate_annotation(file_object, tagged_term, index):
+    """Generates the annotation tag
 
-	Keyword Arguments:
-	file_object -- the file object used for writing 
-	tagged_term -- look up 
-	index -- keep track of the annotation id
-	"""
-	file_object.write("<annotation id=\"%d\">\n" % (index))
-	file_object.write("<infon key=\"title\">%s</infon>\n" %(tagged_term[4]))
-	file_object.write("<location length=\"%d\" offset=\"%d\"></location>\n" % (len(tagged_term[3]),tagged_term[1]))
-	file_object.write("<text>%s</text>\n" % (tagged_term[3]))
+    Keyword Arguments:
+    file_object -- the file object used for writing
+    tagged_term -- look up
+    index -- keep track of the annotation id
+    """
+    file_object.write("<annotation id=\"%d\">\n" % (index))
+    file_object.write("<infon key=\"title\">%s</infon>\n" % (tagged_term[4]))
+    file_object.write("<location length=\"%d\" offset=\"%d\"></location>\n" % (len(tagged_term[3]), tagged_term[1]))
+    file_object.write("<text>%s</text>\n" % (tagged_term[3]))
 
-	# If the annotation type is a Gene,Species, Mutation, SNP
-	# Write out relevant tag
-	if tagged_term[4] == "Gene":
-		file_object.write("<infon key=\"NCBI Gene\">%s</infon>\n" % (tagged_term[5][0:tagged_term[5].find("(")]))
+    # If the annotation type is a Gene,Species, Mutation, SNP
+    # Write out relevant tag
+    if tagged_term[4] == "Gene":
+        file_object.write("<infon key=\"NCBI Gene\">%s</infon>\n" % (tagged_term[5][0:tagged_term[5].find("(")]))
 
-	elif tagged_term[4] == "Species":
-		file_object.write("<infon key=\"NCBI Species\">%s</infon>\n" % (tagged_term[5]))
+    elif tagged_term[4] == "Species":
+        file_object.write("<infon key=\"NCBI Species\">%s</infon>\n" % (tagged_term[5]))
 
-	elif "Mutation" in tagged_term[4]:
-		file_object.write("<infon key=\"tmVar\">%s</infon>\n" % (tagged_term[5]))
+    elif "Mutation" in tagged_term[4]:
+        file_object.write("<infon key=\"tmVar\">%s</infon>\n" % (tagged_term[5]))
 
-	elif "SNP" in tagged_term[4]:
-		file_object.write("<infon key=\"tmVar\">%s</infon>\n" %(tagged_term[5]))
+    elif "SNP" in tagged_term[4]:
+        file_object.write("<infon key=\"tmVar\">%s</infon>\n" % (tagged_term[5]))
 
-	else:
-		# If there is no MESH ID for an annotation	
-		if len(tagged_term) > 5:
-			# check to see if there are multiple mesh tags
-			if "|" in tagged_term[5]:
-				# Write out each MESH id as own tag
-				for ids in tagged_term[5].split("|"):
-					# Some ids dont have the MESH:#### form so added case to that
-					if ":" not in ids:
-						file_object.write("<infon key=\"MESH\">%s</infon>\n" % (tagged_term[5]))
-					else:
-						term_type,term_id = ids.split(":")
-						file_object.write("<infon key=\"%s\">%s</infon>\n" % (term_type,term_id))
-			else:
-				# Some ids dont have the MESH:#### form so added case to that
-				if ":" in tagged_term[5]:
-					term_type,term_id = tagged_term[5].split(":")
-					file_object.write("<infon key=\"%s\">%s</infon>\n" % (term_type,term_id))
-				else:
-					file_object.write("<infon key=\"MESH\">%s</infon>\n" % (tagged_term[5]))
-		else:
-			file_object.write("<infon key=\"MESH\">Unknown</infon>\n")
-	file_object.write("</annotation>\n")
+    else:
+        # If there is no MESH ID for an annotation
+        if len(tagged_term) > 5:
+            # check to see if there are multiple mesh tags
+            if "|" in tagged_term[5]:
+                # Write out each MESH id as own tag
+                for ids in tagged_term[5].split("|"):
+                    # Some ids dont have the MESH:#### form so added case to that
+                    if ":" not in ids:
+                        file_object.write("<infon key=\"MESH\">%s</infon>\n" % (tagged_term[5]))
+                    else:
+                        term_type, term_id = ids.split(":")
+                        file_object.write("<infon key=\"%s\">%s</infon>\n" % (term_type, term_id))
+            else:
+                # Some ids dont have the MESH:#### form so added case to that
+                if ":" in tagged_term[5]:
+                    term_type, term_id = tagged_term[5].split(":")
+                    file_object.write("<infon key=\"%s\">%s</infon>\n" % (term_type, term_id))
+                else:
+                    file_object.write("<infon key=\"MESH\">%s</infon>\n" % (tagged_term[5]))
+        else:
+            file_object.write("<infon key=\"MESH\">Unknown</infon>\n")
+    file_object.write("</annotation>\n")
 
-def convert_pubtator(input_file,output_file):
-	"""Convert pubtators annotation list to XML
 
-	Keyword Arguments:
-	input_file -- the path of pubtators annotation file
-	output_file -- the path to output the converted text
-	"""
-	term_tags = set()
-	with open(output_file,'w') as g:
-		# Header for pubtator's xml format
-		g.write("<collection>\n<source>Pubtator</source>\n")
-		# read from gunzip file
-		with gzip.open(input_file,"rb") as f:
+def convert_pubtator(input_file, output_file):
+    """Convert pubtators annotation list to XML
 
-			for line in tqdm.tqdm(f):
-				#Convert "illegal chracters" (i.e. < > &) in the main text into html entities
-				line = line.strip().replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;').replace('"', '&quot;').replace("'", '&#39;')
+    Keyword Arguments:
+    input_file -- the path of pubtators annotation file
+    output_file -- the path to output the converted text
+    """
+    term_tags = set()
+    with open(output_file, 'w') as g:
+        # Header for pubtator's xml format
+        g.write("<collection>\n<source>Pubtator</source>\n")
+        # read from gunzip file
+        with gzip.open(input_file, "rb") as f:
 
-				# Title parsing
-				if "|t|" in line:
-					title_heading = line.split("|")
-					has_written_title = False
-				
-				# Abstract parsing
-				elif "|a|" in line:
-					abstract_heading = line.split("|")
-					title_offset = len(title_heading[2])
-					has_written_abstract = False
+            for line in tqdm.tqdm(f):
+                # Convert "illegal chracters" (i.e. < > &) in the main text
+                # into html entities
+                line = line.strip().replace('&', '&amp;')
+                line = line.replace('<', '&lt;').replace('>', '&gt;')
+                line = line.replace('"', '&quot;').replace("'", '&#39;')
 
-				# New line means end of current document
-				elif line == "":
-					sorted_tags = sorted(list(term_tags),key=lambda x: x[2])
+                # Title parsing
+                if "|t|" in line:
+                    title_heading = line.split("|")
+                    has_written_title = False
 
-					#Fixing the issue where titles don't have an annotation
-					if len(term_tags) == 0 or sorted_tags[0][2] > title_offset:
-						if not(has_written_title):
-							g.write("<document>\n")
-							g.write("<id>" + title_heading[0] + "</id>\n")
-							g.write("<passage>\n")
-							g.write("<infon key=\"type\">title</infon>\n")
-							g.write("<offset>0</offset>\n")
-							g.write("<text>" + title_heading[2] + "</text>\n")
-							has_written_title = True
+                # Abstract parsing
+                elif "|a|" in line:
+                    abstract_heading = line.split("|")
+                    title_offset = len(title_heading[2])
+                    has_written_abstract = False
 
-					# For every annotation
-					for i,tagged_term in enumerate(sorted_tags):
+                # New line means end of current document
+                elif line == "":
+                    sorted_tags = sorted(list(term_tags), key=lambda x: x[2])
 
-						# Determine if annotation is for the title else abstract
-						if tagged_term[2] <= title_offset:
-							if not(has_written_title):
-								g.write("<document>\n")
-								g.write("<id>" + title_heading[0] + "</id>\n")
-								g.write("<passage>\n")
-								g.write("<infon key=\"type\">title</infon>\n")
-								g.write("<offset>0</offset>\n")
-								g.write("<text>" + title_heading[2] + "</text>\n")
-								has_written_title = True
-							generate_annotation(g,tagged_term,i)
-						# Abastract
-						else:
-							if not(has_written_abstract):
-								g.write("</passage>\n<passage>\n")
-								g.write("<infon key=\"type\">abstract</infon>\n")
-								g.write("<offset>%d</offset>\n" %(title_offset))
-								g.write("<text>" + abstract_heading[2] + "</text>\n")
-								has_written_abstract = True
-							generate_annotation(g,tagged_term,i)
+                    # Fixing the issue where titles don't have an annotation
+                    if len(term_tags) == 0 or sorted_tags[0][2] > title_offset:
+                        if not(has_written_title):
+                            g.write("<document>\n")
+                            g.write("<id>" + title_heading[0] + "</id>\n")
+                            g.write("<passage>\n")
+                            g.write("<infon key=\"type\">title</infon>\n")
+                            g.write("<offset>0</offset>\n")
+                            g.write("<text>" + title_heading[2] + "</text>\n")
+                            has_written_title = True
 
-					# Fixes the issue where articles don't have an abstract
-					if not(has_written_abstract):
-						g.write("</passage>\n<passage>\n")
-						g.write("<infon key=\"type\">abstract</infon>\n")
-						g.write("<offset>%d</offset>\n" %(title_offset))
-						g.write("<text>" + abstract_heading[2] + "</text>\n")
-						has_written_abstract = True
+                    # For every annotation
+                    for i, tagged_term in enumerate(sorted_tags):
 
-					# Close the passage block
-					g.write("</passage>\n</document>\n")
+                        # Determine if annotation is for the title else abstract
+                        if tagged_term[2] <= title_offset:
+                            if not(has_written_title):
+                                g.write("<document>\n")
+                                g.write("<id>" + title_heading[0] + "</id>\n")
+                                g.write("<passage>\n")
+                                g.write("<infon key=\"type\">title</infon>\n")
+                                g.write("<offset>0</offset>\n")
+                                g.write("<text>" + title_heading[2] + "</text>\n")
+                                has_written_title = True
+                            generate_annotation(g, tagged_term, i)
+                        # Abstract
+                        else:
+                            if not(has_written_abstract):
+                                g.write("</passage>\n<passage>\n")
+                                g.write("<infon key=\"type\">abstract</infon>\n")
+                                g.write("<offset>%d</offset>\n" % (title_offset))
+                                g.write("<text>" + abstract_heading[2] + "</text>\n")
+                                has_written_abstract = True
+                            generate_annotation(g, tagged_term, i)
 
-					# Reset the term_Tags
-					term_tags = set([])
+                    # Fixes the issue where articles don't have an abstract
+                    if not(has_written_abstract):
+                        g.write("</passage>\n<passage>\n")
+                        g.write("<infon key=\"type\">abstract</infon>\n")
+                        g.write("<offset>%d</offset>\n" % (title_offset))
+                        g.write("<text>" + abstract_heading[2] + "</text>\n")
+                        has_written_abstract = True
 
-				# Compile each term into a set of terms
-				else:
-					terms = line.split("\t")
-					terms[1] = int(terms[1])
-					terms[2] = int(terms[2])
-					term_tags.add(terms)
-		# Close the whole collection
-		g.write("</collection>\n")
+                    # Close the passage block
+                    g.write("</passage>\n</document>\n")
 
-convert_pubtator("bioconcepts2pubtator_offsets.gz","pubmed_docs.xml")
+                    # Reset the term_Tags
+                    term_tags = set([])
+
+                # Compile each term into a set of terms
+                else:
+                    terms = line.split("\t")
+                    terms[1] = int(terms[1])
+                    terms[2] = int(terms[2])
+                    term_tags.add(terms)
+        # Close the whole collection
+        g.write("</collection>\n")
+
+convert_pubtator("bioconcepts2pubtator_offsets.gz", "pubmed_docs.xml")
