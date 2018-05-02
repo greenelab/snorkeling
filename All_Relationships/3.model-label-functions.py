ca198e30a3e011da91843eec85418ba923cc2dda
David Nicholson <dnicholson329@gmail.com>
2018-05-02 12:08:17 -0400

    Updated environment and added sentence-label-dev.xlsx

--- All_Relationships/3.model-label-functions.py
+++ All_Relationships/3.model-label-functions.py
@@ -81,7 +81,7 @@ else:
 
 # This code will load the label matrix that was generated in the previous notebook ([Notebook 2](2.data-labeler.ipynb)). **Disclaimer**: this block might break, which means that the snorkel code is still using its old code. The problem with the old code is that sqlalchemy will attempt to load all the labels into memory. Doesn't sound bad if you keep the amount of labels small, but doesn't scale when the amount of labels increases exponentially. Good news is that there is a pull request to fix this issue. [Check it out here!](https://github.com/HazyResearch/snorkel/pull/789)
 
-# In[6]:
+# In[ ]:
 
 
 from snorkel.annotations import load_gold_labels
@@ -98,32 +98,40 @@ L_gold_dev = load_gold_labels(session, annotator_name='danich1', cids_query=cids
 annotated_cands_dev_ids = list(map(lambda x: L_gold_dev.row_index[x], L_gold_dev.nonzero()[0]))
 
 
-# In[7]:
+# In[ ]:
 
 
 L_gold_dev
 
 
-# In[8]:
+# In[16]:
 
 
-get_ipython().run_cell_magic('time', '', 'labeler = LabelAnnotator(lfs=[])\n\n# Only grab candidates that have human labels\n#cids = session.query(Candidate.id).filter(Candidate.id.in_(annotated_cands_train_ids))\nL_train = labeler.load_matrix(session, split=0) #\n\ncids = session.query(Candidate.id).filter(Candidate.id.in_(gold_cids))\nL_dev = labeler.load_matrix(session,cids_query=cids)')
-
-
-# In[9]:
-
-
-type(L_train)
+train_candidate_ids = np.loadtxt('data/labeled_candidates.txt').astype(int).tolist()
+train_candidate_ids
 
 
 # In[10]:
+
+
+dev_candidate_ids = np.loadtxt('data/labeled_dev_candidates.txt').astype(int).tolist()
+dev_candidate_ids
+
+
+# In[11]:
+
+
+get_ipython().run_cell_magic('time', '', 'labeler = LabelAnnotator(lfs=[])\n\n# Only grab candidates that have human labels\ncids = session.query(Candidate.id).filter(Candidate.id.in_(train_candidate_ids))\nL_train = labeler.load_matrix(session, split=0) #\n\ncids = session.query(Candidate.id).filter(Candidate.id.in_(dev_candidate_ids))\nL_dev = labeler.load_matrix(session,cids_query=cids)')
+
+
+# In[12]:
 
 
 print("Total Data Shape:")
 print(L_train.shape)
 
 
-# In[11]:
+# In[13]:
 
 
 L_train = L_train[np.unique(L_train.nonzero()[0]), :]
@@ -131,29 +139,29 @@ print("Total Data Shape:")
 print(L_train.shape)
 
 
-# In[12]:
+# In[14]:
 
 
-type(L_train)
+L_dev.shape
 
 
 # # Train the Generative Model
 
 # Here is the first step of classification step of this project, where we train a gnerative model to discriminate the correct label each candidate will receive. Snorkel's generative model uses a Gibbs Sampling on a [factor graph](http://deepdive.stanford.edu/assets/factor_graph.pdf), to generate the probability of a potential candidate being a true candidate (label of 1).
 
-# In[13]:
+# In[17]:
 
 
 get_ipython().run_cell_magic('time', '', 'from snorkel.learning import GenerativeModel\n\ngen_model = GenerativeModel()\ngen_model.train(\n    L_train,\n    epochs=30,\n    decay=0.95,\n    step_size=0.1 / L_train.shape[0],\n    reg_param=1e-6,\n    threads=50,\n    verbose=True\n)')
 
 
-# In[14]:
+# In[18]:
 
 
 gen_model.weights.lf_accuracy
 
 
-# In[15]:
+# In[19]:
 
 
 from utils.disease_gene_lf import LFS
@@ -162,19 +170,19 @@ learned_stats_df.index = list(LFS)
 learned_stats_df
 
 
-# In[16]:
+# In[20]:
 
 
 get_ipython().run_line_magic('time', 'train_marginals = gen_model.marginals(L_train)')
 
 
-# In[17]:
+# In[ ]:
 
 
 print(len(train_marginals[train_marginals > 0.5]))
 
 
-# In[18]:
+# In[ ]:
 
 
 plt.hist(train_marginals, bins=20)
@@ -182,25 +190,35 @@ plt.title("Training Marginals for Gibbs Sampler")
 plt.show()
 
 
-# In[19]:
+# ## ROC of Generative Model
 
-
-tp, fp, tn, fn = gen_model.error_analysis(session, L_dev, L_gold_dev)
-
-
-# In[20]:
+# In[21]:
 
 
 dev_marginals = gen_model.marginals(L_dev)
 
 
-# In[21]:
+# In[ ]:
 
 
 fpr, tpr, threshold = roc_curve(L_gold_dev.todense(), dev_marginals)
 plt.plot([0,1], [0,1])
 plt.plot(fpr, tpr, label='AUC {:.2f}'.format(auc(fpr, tpr)))
 plt.legend()
+
+
+# In[ ]:
+
+
+L_dev.lf_stats(session, L_gold_dev[L_gold_dev!=0].T, gen_model.learned_lf_stats()['Accuracy'])
+
+
+# ## Individual Candidate Error Analysis
+
+# In[ ]:
+
+
+tp, fp, tn, fn = gen_model.error_analysis(session, L_dev, L_gold_dev)
 
 
 # In[ ]:
@@ -230,13 +248,29 @@ c = sv.get_selected() if sv else list(fp.union(fn))[0]
 c
 
 
+# In[ ]:
+
+
+c.labels
+
+
+# ## Generate Excel File of Train Data
+
 # In[22]:
 
 
+pair_df = pd.read_csv("data/disease-gene-pairs-association.csv.xz", compression='xz')
+pair_df.head(2)
+
+
+# In[23]:
+
+
 rows = list()
-for i in range(L_train.shape[0]):
+for i in tqdm.tqdm(range(L_dev.shape[0])):
     row = OrderedDict()
-    candidate = L_train.get_candidate(session, i)
+    candidate = L_dev.get_candidate(session, i)
+    row['candidate_id'] = candidate.id
     row['disease'] = candidate[0].get_span()
     row['gene'] = candidate[1].get_span()
     row['doid_id'] = candidate.Disease_cid
@@ -245,54 +279,45 @@ for i in range(L_train.shape[0]):
     row['label'] = train_marginals[i]
     rows.append(row)
 sentence_df = pd.DataFrame(rows)
+sentence_df['entrez_gene_id'] = sentence_df.entrez_gene_id.astype(int)
+sentence_df.head(2)
+
+
+# In[24]:
+
+
+sentence_df = pd.merge(
+    sentence_df,
+    pair_df[["doid_id", "entrez_gene_id", "doid_name", "gene_symbol"]],
+    on=["doid_id", "entrez_gene_id"],
+    how="left"
+)
+sentence_df.head(2)
+
+
+# In[25]:
+
 
 sentence_df = pd.concat([
     sentence_df,
-    pd.DataFrame(L_train.todense(), columns=list(LFS))
+    pd.DataFrame(L_dev.todense(), columns=list(LFS))
 ], axis='columns')
 
 sentence_df.tail()
 
 
-# In[23]:
+# In[26]:
 
 
-writer = pd.ExcelWriter('data/sentence-labels.xlsx')
-sentence_df.to_excel(writer, sheet_name='sentences', index=False)
+writer = pd.ExcelWriter('data/sentence-labels-dev.xlsx')
+(sentence_df
+ .sort_values("label", ascending=False)
+ .to_excel(writer, sheet_name='sentences', index=False)
+)
 if writer.engine == 'xlsxwriter':
     for sheet in writer.sheets.values():
         sheet.freeze_panes(1, 0)
 writer.close()
-
-
-# In[ ]:
-
-
-sentence_df
-
-
-# In[ ]:
-
-
-c.labels
-
-
-# In[ ]:
-
-
-L_dev.get_row_index(c)
-
-
-# In[ ]:
-
-
-dev_marginals[26]
-
-
-# In[ ]:
-
-
-L_dev.lf_stats(session, L_gold_dev[L_gold_dev!=0].T, gen_model.learned_lf_stats()['Accuracy'])
 
 
 # # Save Training Marginals
