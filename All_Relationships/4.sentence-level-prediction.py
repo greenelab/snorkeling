c4f653bb284c1c8fd39736f5d9d414cbb5f47663
David Nicholson <dnicholson329@gmail.com>
2018-05-02 12:16:17 -0400

    Refactored code to efficiently run LSTM on cluster

--- All_Relationships/4.sentence-level-prediction.py
+++ All_Relationships/4.sentence-level-prediction.py
@@ -76,40 +76,140 @@ else:
     print("Please pick a valid edge type")
 
 
-# # Load preprocessed data 
+# ## Train a LSTM Disc Model
 
-# This code will automatically load our labels and features that were generated in the [previous notebook](2.data-labeler.ipynb). 
+# This block of code trains an LSTM. An LSTM is a special type of recurrent nerual network that retains a memory of past values over period of time. ([Further explaination here](http://colah.github.io/posts/2015-08-Understanding-LSTMs/)). The problem with the code below is that sqlalchemy runs into an out of memory error on my computer during the preprocessing step. As a consequence we have to resort loading this data onto University of Pennsylvania's Performance Computing Cluster. The data that gets preprocessed is exported to a text file and then get shipped towards the cluster.
 
-# In[ ]:
-
-
-get_ipython().run_cell_magic('time', '', 'labeler = LabelAnnotator(lfs=[])\n\nL_train = labeler.load_matrix(session, split=0)\nL_dev = labeler.load_matrix(session, split=1)\nL_test = labeler.load_matrix(session, split=2)')
-
+# ## Write the Training data to an External File
 
 # In[ ]:
 
 
-print "Total Data Shape:"
-print L_train.shape
-print L_dev.shape
-print L_test.shape
-print
+train_sentences_df = pd.read_excel("data/sentence-labels.xlsx")
+train_sentences_df.head(2)
 
 
 # In[ ]:
 
 
-get_ipython().run_cell_magic('time', '', 'featurizer = FeatureAnnotator()\n\nF_train = featurizer.load_matrix(session, split=0)\nF_dev = featurizer.load_matrix(session, split=1)\nF_test = featurizer.load_matrix(session, split=2)')
+candidate_ids = train_sentences_df.candidate_id.astype(int).tolist()
+candidate_ids
 
 
 # In[ ]:
 
 
-print "Total Data Shape:"
-print F_train.shape
-print F_dev.shape
-print F_test.shape
-print
+train_cands = (
+    session
+    .query(Candidate)
+    .filter(Candidate.id.in_(candidate_ids))
+    .all() 
+)
+
+
+# In[ ]:
+
+
+lstm = reRNN(seed=100, n_threads=20)
+
+
+# In[ ]:
+
+
+rows = []
+for c in tqdm.tqdm(train_cands):
+    data, ends = lstm._preprocess_data([c], extend=True)
+    rows.append({
+        "data_str": ",".join([str(x) for x in data[0]]),
+        "ends": ends[0],
+        "candidate_id": c.id
+    })
+train_data = pd.DataFrame(rows)
+train_data.head(2)
+
+
+# In[ ]:
+
+
+train_data = pd.merge(
+    train_data,
+    train_sentences_df[["candidate_id", "label"]],
+    how='left'
+)
+train_data.head(2)
+
+
+# In[ ]:
+
+
+train_data.to_csv("data/lstm/train_data.tsv", sep="\t", index=False)
+
+
+# ### Save the word dictionary to an External File
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('time', '', 'with open("data/lstm/train_word_dict.csv", \'w\') as f:\n    output = csv.DictWriter(f, fieldnames=["Key", "Value"])\n    output.writeheader()\n    for key in tqdm.tqdm(lstm.word_dict.d):\n        output.writerow({\'Key\':key, \'Value\': lstm.word_dict.d[key]})')
+
+
+# ## Write Dev data to an External File
+
+# In[ ]:
+
+
+dev_sentences_df = pd.read_excel("data/sentence-labels-dev.xlsx")
+dev_sentences_df.head(2)
+
+
+# In[ ]:
+
+
+candidate_ids = dev_sentences_df.candidate_id.astype(int).tolist()
+candidate_ids
+
+
+# In[ ]:
+
+
+dev_cands = (
+    session
+    .query(Candidate)
+    .filter(Candidate.id.in_(candidate_ids))
+    .all() 
+)
+
+
+# In[ ]:
+
+
+rows = []
+for c in tqdm.tqdm(dev_cands):
+    data, ends = lstm._preprocess_data([c])
+    rows.append({
+        "data_str": ",".join([str(x) for x in data[0]]),
+        "ends": ends[0],
+        "candidate_id": c.id
+    })
+dev_data = pd.DataFrame(rows)
+dev_data.head(2)
+
+
+# In[ ]:
+
+
+dev_data = pd.merge(
+    dev_data,
+    dev_sentences_df[["candidate_id", "label"]],
+    how='left'
+)
+dev_data.head(2)
+
+
+# In[ ]:
+
+
+dev_data.to_csv("data/lstm/dev_data.tsv", sep="\t", index=False)
 
 
 # # Train Sparse Logistic Regression Disc Model
@@ -119,65 +219,41 @@ print
 # In[ ]:
 
 
-L_gold_dev = load_gold_labels(session, annotator_name='danich1', split=1)
-L_gold_train = load_gold_labels(session, annotator_name='danich1', split=0)
+train_sentences_df = pd.read_excel("data/sentence-labels.xlsx")
+train_sentences_df.head(2)
 
 
 # In[ ]:
 
 
-annotated_cands_train_ids = list(map(lambda x: L_gold_train.row_index[x],  L_gold_train.nonzero()[0]))
-annotated_cands_dev_ids = list(map(lambda x: L_gold_dev.row_index[x],L_gold_dev.nonzero()[0]))
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(
+    train_sentences_df.sentence.values
+)
+X
 
 
 # In[ ]:
 
 
-labeler = LabelAnnotator(lfs=[])
-cids = session.query(Candidate.id).filter(Candidate.id.in_(annotated_cands_train_ids))
-L_train = labeler.load_matrix(session,cids_query=cids)
+labels = [
+    train_sentences_df.label.apply(lambda x: 1 if x > 0.5 else 0)
+]
 
-
-# In[ ]:
-
-
-words = pd.read_csv("vanilla_lstm/lstm_disease_gene_holdout/train_word_dict.csv")
-train_data = pd.read_csv("vanilla_lstm/lstm_disease_gene_holdout/train_candidates_to_ids.csv")
-dev_data = pd.read_csv("vanilla_lstm/lstm_disease_gene_holdout/dev_candidates_to_ids.csv")
-#test_data = pd.read_csv("vanilla_lstm/lstm_disease_gene_holdout/test_candidates_to_ids.csv")
-
-human_cur_labels = list(L_gold_train[L_gold_train != 0].toarray()[0])
-sen_het_labels = np.loadtxt("vanilla_lstm/lstm_disease_gene_holdout/subsampled/train_marginals_subsampled.txt")
-sen_het_labels = list(map(lambda x: 1 if x > 0.5 else -1, sen_het_labels))
-
-
-# In[ ]:
-
-
-vectorizer = CountVectorizer(vocabulary=words["Key"].drop_duplicates())
-X = vectorizer.fit_transform(train_data.query("id in @annotated_cands_train_ids").sort_values("id")["sentence"].values)
-dev_X = vectorizer.transform(dev_data.query("id in @annotated_cands_dev_ids").sort_values("id")["sentence"].values)
-#test_X = vectorizer.transform(test_data["sentence"].values)
+model_labels = [
+    "all_LF_LR"
+]
+    
+lr_grids = [
+    {'C':np.linspace(1,10, num=4)} for _ in range(len(labels))
+]
+final_models = []
 
 
 # In[ ]:
 
 
 lr_model = LogisticRegression()
-labels = [
-    human_cur_labels,
-    sen_het_labels
-]
-
-model_labels = [
-    "hand_LR", 
-    "all_LF_LR"
-]
-    
-lr_grids = [
-    {'C':np.linspace(1,10, num=100)} for _ in range(len(labels))
-]
-final_models = []
 
 
 # In[ ]:
@@ -240,134 +316,4 @@ for marginal, model_label in zip(lr_marginals,model_labels):
     pd.DataFrame(marginal,
              columns=["LR_Marginals"]
         ).to_csv(filename, index=False)
-
-
-# # DO NOT RUN  BELOW
-
-# ## Train a LSTM Disc Model
-
-# This block of code trains an LSTM. An LSTM is a special type of recurrent nerual network that retains a memory of past values over period of time. ([Further explaination here](http://colah.github.io/posts/2015-08-Understanding-LSTMs/)). The problem with the code below is that sqlalchemy runs into an out of memory error on my computer during the preprocessing step. As a consequence we have to resort loading this data onto University of Pennsylvania's Performance Computing Cluster. The data that gets preprocessed is exported to a text file and then get shipped towards the cluster.
-
-# In[ ]:
-
-
-directory = 'vanilla_lstm/lstm_disease_gene_holdout/subsampled'
-
-
-# In[ ]:
-
-
-get_ipython().run_line_magic('time', 'train_marginals = load_marginals(session, split=0)')
-np.savetxt("{}/train_marginals".format(directory), train_marginals)
-
-
-# In[ ]:
-
-
-def read_word_dict(filename):
-    """
-     Read a CSV into a dictionary using the Key column (as string) and Value column (as int).
-    Keywords:
-    fielname - name of the file to read
-    """
-    data = {}
-    with open(filename, 'r') as f:
-        input_file = csv.DictReader(f)
-        for row in tqdm.tqdm(input_file):
-            data[row['Key']] = int(row['Value'])
-    return data
-
-
-# In[ ]:
-
-
-get_ipython().run_cell_magic('time', '', 'from snorkel.learning.disc_models.rnn.utils import SymbolTable\n\n"""\ntrain_kwargs = {\n    \'lr\':         0.001,\n    \'dim\':        100,\n    \'n_epochs\':   10,\n    \'dropout\':    0.5,\n    \'print_freq\': 1,\n    \'max_sentence_length\': 1000,\n}\n"""\nword_dict = read_word_dict("vanilla_lstm/lstm_disease_gene_holdout/train_word_dict.csv")\nlstm = reRNN(seed=100, n_threads=4)\nlstm.word_dict = SymbolTable()\nlstm.word_dict.d = word_dict\n#lstm.train(train_cands, train_marginals[0:10], X_dev=dev_cands, Y_dev=L_dev[0:10], **train_kwargs)')
-
-
-# ### Write the Training data to an External File
-
-# In[ ]:
-
-
-get_ipython().run_cell_magic('time', '', 'field_names = [\n    "disease_id", "disease_name",\n    "disease_char_start", "disease_char_end",\n    "gene_id", "gene_name",\n    "gene_char_start", "gene_char_end",\n    "sentence", "pubmed"\n]\nchunksize = 100000\nstart = 0\n\nwith open(\'{}/train_candidates_ends.csv\'.format(directory), \'w\') as g:\n    with open("{}/train_candidates_offsets.csv".format(directory), "w") as f:\n        with open("{}/train_candidates_sentences.csv".format(directory), "w") as h:\n            output = csv.writer(f)\n            writer = csv.DictWriter(h, fieldnames=field_names)\n            writer.writeheader()\n\n            while True:\n                train_cands = (\n                        session\n                        .query(DiseaseGene)\n                        .filter(DiseaseGene.split == 0)\n                        .order_by(DiseaseGene.id)\n                        .limit(chunksize)\n                        .offset(start)\n                        .all()\n                )\n\n                if not train_cands:\n                    break\n\n                for c in tqdm.tqdm(train_cands):\n                    data, ends = lstm._preprocess_data([c], extend=True)\n                    output.writerow(data[0])\n                    g.write("{}\\n".format(ends[0]))\n\n                    row = {\n                    "disease_id": c.Disease_cid,"disease_name":c[0].get_span(),\n                    "disease_char_start":c[0].char_start, "disease_char_end": c[0].char_end, \n                    "gene_id": c.Gene_cid, "gene_name":c[1].get_span(), \n                    "gene_char_start":c[1].char_start, "gene_char_end":c[1].char_end, \n                    "sentence": c.get_parent().text, "pubmed": c.get_parent().get_parent().name\n                    }\n\n                    writer.writerow(row)\n\n                start += chunksize')
-
-
-# ### Save the word dictionary to an External File
-
-# In[ ]:
-
-
-get_ipython().run_cell_magic('time', '', 'with open("{}/train_word_dict.csv".format(directory), \'w\') as f:\n    output = csv.DictWriter(f, fieldnames=["Key", "Value"])\n    output.writeheader()\n    for key in tqdm.tqdm(lstm.word_dict.d):\n        output.writerow({\'Key\':key, \'Value\': lstm.word_dict.d[key]})')
-
-
-# ### Save the Development Candidates to an External File
-
-# In[ ]:
-
-
-dev_cands = (
-    session
-    .query(DiseaseGene)
-    .filter(DiseaseGene.id.in_(list(map(int,train_data.sort_values("id")["id"].values))))
-    .all()
-)
-dev_cand_labels = pd.read_csv("stratified_data/train_set.csv")
-hetnet_set = set(map(tuple,dev_cand_labels[dev_cand_labels["hetnet"] == 1][["disease_id", "gene_id"]].values))
-
-
-# In[ ]:
-
-
-dev_cands = (
-    session
-    .query(DiseaseGene)
-    .filter(DiseaseGene.id.in_(annotated_cands))
-    .all()
-)
-dev_cand_labels = pd.read_csv("stratified_data/dev_set.csv")
-hetnet_set = set(map(tuple,dev_cand_labels[dev_cand_labels["hetnet"] == 1][["disease_id", "gene_id"]].values))
-
-
-# In[ ]:
-
-
-dev_cands = (
-        session
-        .query(DiseaseGene)
-        .filter(DiseaseGene.split == 1)
-        .order_by(DiseaseGene.id)
-        .all()
-)
-
-dev_cand_labels = pd.read_csv("stratified_data/dev_set.csv")
-hetnet_set = set(map(tuple,dev_cand_labels[dev_cand_labels["hetnet"] == 1][["disease_id", "gene_id"]].values))
-
-
-# In[ ]:
-
-
-get_ipython().run_cell_magic('time', '', 'field_names = [\n    "disease_id", "disease_name",\n    "disease_char_start", "disease_char_end",\n    "gene_id", "gene_name",\n    "gene_char_start", "gene_char_end",\n    "sentence", "pubmed"\n]\n\nwith open(\'{}/delete_candidates_offset.csv\'.format(directory), \'w\') as g:\n    with open(\'{}/train_candidates_ends.csv\'.format(directory), \'w\') as f:\n        with open(\'{}/delete_candidates_sentences.csv\'.format(directory), \'w\') as h:\n            \n            output = csv.writer(g)\n            #label_output = csv.writer(f)\n            writer = csv.DictWriter(h, fieldnames=field_names)\n            writer.writeheader()\n            \n            for c in tqdm.tqdm(dev_cands):\n                data, ends = lstm._preprocess_data([c])\n                output.writerow(data[0])\n                f.write("{}\\n".format(ends[0]))\n                #label_output.writerow([1 if (c.Disease_cid, int(c.Gene_cid)) in hetnet_set else -1])\n\n                row = {\n                "disease_id": c.Disease_cid,"disease_name":c[0].get_span(),\n                "disease_char_start":c[0].char_start, "disease_char_end": c[0].char_end, \n                "gene_id": c.Gene_cid, "gene_name":c[1].get_span(), \n                "gene_char_start":c[1].char_start, "gene_char_end":c[1].char_end, \n                "sentence": c.get_parent().text, "pubmed": c.get_parent().get_parent().name\n                }\n\n                writer.writerow(row) ')
-
-
-# ### Save the Test Candidates to an External File
-
-# In[ ]:
-
-
-test_cands = (
-        session
-        .query(DiseaseGene)
-        .filter(DiseaseGene.split == 2)
-        .order_by(DiseaseGene.id)
-        .all()
-)
-
-dev_cand_labels = pd.read_csv("stratified_data/test_set.csv")
-hetnet_set = set(map(tuple,dev_cand_labels[dev_cand_labels["hetnet"] == 1][["disease_id", "gene_id"]].values))
-
-
-# In[ ]:
-
-
-get_ipython().run_cell_magic('time', '', 'field_names = [\n    "disease_id", "disease_name",\n    "disease_char_start", "disease_char_end",\n    "gene_id", "gene_name",\n    "gene_char_start", "gene_char_end",\n    "sentence", "pubmed"\n]\n\nwith open(\'{}/test_candidates_offset.csv\'.format(directory), \'w\') as g:\n    with open(\'{}/test_candidates_labels.csv\'.format(directory), \'w\') as f:\n        with open(\'{}/test_candidates_sentences.csv\'.format(directory), \'w\') as h:\n            \n            output = csv.writer(g)\n            label_output = csv.writer(f)\n            writer = csv.DictWriter(h, fieldnames=field_names)\n            writer.writeheader()\n            \n            for c in tqdm.tqdm(test_cands):\n                data, ends = lstm._preprocess_data([c])\n                output.writerow(data[0])\n                label_output.writerow([1 if (c.Disease_cid, int(c.Gene_cid)) in hetnet_set else -1])\n\n                row = {\n               "disease_id": c.Disease_cid,"disease_name":c[0].get_span(),\n                "disease_char_start":c[0].char_start, "disease_char_end": c[0].char_end, \n                "gene_id": c.Gene_cid, "gene_name":c[1].get_span(), \n                "gene_char_start":c[1].char_start, "gene_char_end":c[1].char_end, \n                "sentence": c.get_parent().text, "pubmed": c.get_parent().get_parent().name\n                }\n\n                writer.writerow(row) ')
 
