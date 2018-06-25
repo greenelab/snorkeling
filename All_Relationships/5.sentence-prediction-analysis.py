1e84e09b97b80a99824c6cce4b50411661da9f84
David Nicholson <dnicholson329@gmail.com>
2018-06-25 13:13:57 -0400

    Adding ROC curves and p-values

--- All_Relationships/5.sentence-prediction-analysis.py
+++ All_Relationships/5.sentence-prediction-analysis.py
@@ -12,9 +12,9 @@
 # In[1]:
 
 
-get_ipython().run_line_magic('load_ext', 'autoreload')
-get_ipython().run_line_magic('autoreload', '2')
-get_ipython().run_line_magic('matplotlib', 'inline')
+get_ipython().magic(u'load_ext autoreload')
+get_ipython().magic(u'autoreload 2')
+get_ipython().magic(u'matplotlib inline')
 
 import csv
 import glob
@@ -25,8 +25,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import re
 import pandas as pd
+from scipy.stats import norm
 import seaborn as sns
-from sklearn.metrics import average_precision_score, precision_recall_curve, roc_curve, auc, f1_score, confusion_matrix
+from sklearn.metrics import average_precision_score, precision_recall_curve, roc_curve
+from sklearn.metrics import auc, f1_score, confusion_matrix
 import tqdm
 
 
@@ -49,25 +51,25 @@ session = SnorkelSession()
 # In[3]:
 
 
-from snorkel.annotations import FeatureAnnotator, LabelAnnotator, load_marginals
-from snorkel.learning import SparseLogisticRegression
-from snorkel.learning.disc_models.rnn import reRNN
-from snorkel.learning.utils import RandomSearch
-from snorkel.models import Candidate, FeatureKey, candidate_subclass
-from snorkel.utils import get_as_dict
-from snorkel.viewer import SentenceNgramViewer
-from sqlalchemy import and_
-from tree_structs import corenlp_to_xmltree
-from treedlib import compile_relation_feature_generator
+import sys
+sys.path.append('/home/danich1/Documents/snorkeling/snorkel/treedlib/treedlib')
 
 
 # In[4]:
 
 
-edge_type = "dg"
+from snorkel.annotations import FeatureAnnotator, LabelAnnotator, load_marginals
+from snorkel.models import Candidate, candidate_subclass
+from snorkel.viewer import SentenceNgramViewer
 
 
 # In[5]:
+
+
+edge_type = "dg"
+
+
+# In[6]:
 
 
 if edge_type == "dg":
@@ -86,76 +88,48 @@ else:
 
 # Here is where we load the test dataset in conjunction with the previously trained disc models. Each algorithm will output a probability of a candidate being a true candidate.
 
-# In[6]:
-
-
-from snorkel.annotations import load_gold_labels
-
-L_gold_dev = load_gold_labels(session, annotator_name='danich1', split=1)
-Y = L_gold_dev[L_gold_dev != 0].todense()
-
-
 # In[7]:
 
 
-print(pd.Series(Y.tolist()[0]).value_counts())
+dev_sentence_df = pd.read_excel("data/sentence-labels-dev.xlsx")
+dev_sentence_df = dev_sentence_df[dev_sentence_df.curated_dsh.notnull()]
+dev_sentence_df.head(2)
 
 
 # In[8]:
 
 
-marginal_files = [
-    "vanilla_lstm/lstm_disease_gene_holdout/subsampled/lf_marginals/hand_LR_dev_marginals.csv",
-    "vanilla_lstm/lstm_disease_gene_holdout/subsampled/lf_marginals/all_LF_LR_dev_marginals.csv"
-]
-
-file_labels = [
-    "HUMAN_BW_LR",
-    "LF_BW_LR"
-]
-
-model_labels = [
-    "HUMAN_BW_LR",
-    "LF_BW_LR",
-]
-
-model_colors = [
-    "red",
-    "blue",
-]
+marginals_df = pd.read_table('data/disc_model_marginals.tsv')
+marginals_df.head(2)
 
 
 # In[9]:
 
 
-model_marginals = pd.DataFrame(Y.T, columns=["True_Labels"])
-for marginal_label, marginal_file in zip(file_labels, marginal_files):
-    model_marginals[marginal_label] = pd.read_csv(marginal_file)
-
-model_marginals.head(10)
-
-
-# In[10]:
-
-
-#test_set = pd.read_csv("stratified_data/lstm_disease_gene_holdout/test_candidates_sentences.csv")
+dev_sentence_df.curated_dsh.value_counts()
 
 
 # # Accuracy ROC
 
 # From the probabilities calculated above, we can create a [Receiver Operator Curve](http://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html) (ROC) graph to measure the false positive rate and the true positive rate at each calculated threshold.
 
+# In[10]:
+
+
+model_aucs = {}
+
+
 # In[11]:
 
 
-marginal_labels = [col for col in model_marginals.columns if col != "True_Labels"]
 plt.figure(figsize=(7,5))
 plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--', label="Random")
 
-for marginal_label, model_label, model_color in zip(marginal_labels, model_labels, model_colors):
-    fpr, tpr, _= roc_curve(model_marginals["True_Labels"], model_marginals[marginal_label])
+for model in marginals_df.columns:
+    fpr, tpr, _= roc_curve(dev_sentence_df.curated_dsh.values, marginals_df[model])
     model_auc = auc(fpr, tpr)
-    plt.plot(fpr, tpr, color=model_color, label="{} (area = {:0.2f})".format(model_label, model_auc))
+    model_aucs[model] = model_auc
+    plt.plot(fpr, tpr, label="{} (AUC = {:0.2f})".format(model, model_auc))
 
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
@@ -163,21 +137,61 @@ plt.title('Accuracy ROC')
 plt.legend(loc="lower right")
 
 
-# # Precision vs Recall Curve
+# ## Statistical Significance for ROC
 
-# This code produces a [Precision-Recall](http://scikit-learn.org/stable/auto_examples/model_selection/plot_precision_recall.html) graph, which shows the trade off between [precision and recall](https://en.wikipedia.org/wiki/Precision_and_recall) at each given probability threshold.
+# This block of code calculates the p-value for each ROC curve above. Each AUC is equivalent to the Mann-Whitney U statistic (aka Willcoxon Rank Sum Test). Calulating this statistic is easy and the equation can be found [here](https://www.quora.com/How-is-statistical-significance-determined-for-ROC-curves-and-AUC-values). Since more than 20 data points were used to generate the above ROC curves, normal approximation canm used to calculate the p-values. The equaltions for the normal approximation can be found [here](https://en.wikipedia.org/wiki/Mann%E2%80%93Whitney_U_test). Looking at the bottom dataframe, one can see there isn't strong evidence that the generated ROCs are greater than 0.50.
 
 # In[12]:
 
 
-marginal_labels = [col for col in model_marginals.columns if col != "True_Labels"]
+class_dist_df = dev_sentence_df.curated_dsh.value_counts()
+n1 = class_dist_df[0]
+n2 = class_dist_df[1]
+mu = (n1*n2)/2
+sigma_u = np.sqrt((n1 * n2 * (n1+n2+1))/12)
+print("mu: {:f}, sigma: {:f}".format(mu, sigma_u))
+
+
+# In[13]:
+
+
+model_auc_df = pd.DataFrame.from_dict(model_aucs, orient='index')
+model_auc_df = model_auc_df.rename(index=str, columns={0:'auroc'})
+model_auc_df.head(2)
+
+
+# In[14]:
+
+
+model_auc_df['u'] = model_auc_df.auroc.apply(lambda x: x*n1*n2)
+model_auc_df['z_u'] = model_auc_df.u.apply(lambda z_u: (z_u- mu)/sigma_u)
+model_auc_df.head(2)
+
+
+# In[15]:
+
+
+model_auc_df['p_value'] = model_auc_df.z_u.apply(lambda z_u: norm.sf(z_u, loc=0, scale=1))
+model_auc_df
+
+
+# # Precision vs Recall Curve
+
+# This code produces a [Precision-Recall](http://scikit-learn.org/stable/auto_examples/model_selection/plot_precision_recall.html) graph, which shows the trade off between [precision and recall](https://en.wikipedia.org/wiki/Precision_and_recall) at each given probability threshold.
+
+# In[16]:
+
+
 plt.figure(figsize=(7,5))
 
-for marginal_label, model_label, model_color in zip(marginal_labels, model_labels, model_colors):
-    precision, recall, _ = precision_recall_curve(model_marginals["True_Labels"], model_marginals[marginal_label])
-    model_precision = average_precision_score(model_marginals["True_Labels"], model_marginals[marginal_label])
-    model_f1 = f1_score(model_marginals["True_Labels"], list(map(lambda x: 1 if x > 0.5 else -1, model_marginals[marginal_label])))
-    plt.plot(recall, precision, color=model_color, label="{} curve (F1 = {:0.2f})".format(model_label, model_f1))
+positive_class = dev_sentence_df.curated_dsh.values.sum()/dev_sentence_df.shape[0]
+plt.plot([0,1], [positive_class, positive_class], color='grey', 
+         linestyle='--', label='Baseline (AUC = {:0.2f})'.format(positive_class))
+
+for model in marginals_df.columns:
+    precision, recall, _ = precision_recall_curve(dev_sentence_df.curated_dsh.values, marginals_df[model])
+    model_auc = auc(recall, precision)
+    plt.plot(recall, precision, label="{} curve (AUC = {:0.2f})".format(model, model_auc))
 
 plt.ylabel('Precision')
 plt.xlabel('Recall')
@@ -189,7 +203,7 @@ plt.legend(loc="lower right")
 
 # # LSTM BENCHMARKING
 
-# In[13]:
+# In[ ]:
 
 
 import glob
@@ -225,7 +239,7 @@ f.suptitle("LSTM Benchmark", fontsize=14)
 plt.show()
 
 
-# In[14]:
+# In[ ]:
 
 
 import glob
