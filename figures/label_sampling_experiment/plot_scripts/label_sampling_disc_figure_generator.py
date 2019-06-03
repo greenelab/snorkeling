d8c6fd30d4ae4a675f1aa52ce71f10c094aae837
David Nicholson <dnicholson329@gmail.com>
2019-06-03 15:17:03 -0400

    Made changes per aljee2 and cgreene's comments

--- figures/label_sampling_experiment/plot_scripts/label_sampling_disc_figure_generator.py
+++ figures/label_sampling_experiment/plot_scripts/label_sampling_disc_figure_generator.py
@@ -8,18 +8,58 @@ import pdb
 
 plt.switch_backend('agg')
 
-def get_dataframes(result_dir, file_path):
+def get_dataframes(
+    result_dir, file_path,
+    starting_point=0,
+    ending_point=30,
+    step=5, num_of_points=4
+):
+    """
+     This function grabs the result tsv files
+     and loads then into a dictionary strucutre
+     [relationship] -> dataframe
+
+     Args:
+         result_dir - the directory containing all the results
+         file_path - the path to extract the result files
+         starting_point - the point to start each subgraph with in plot_graph function
+         ending_point - the point to end each subgraph with
+         step - the number to increase the middle points with
+         num_of_points - the number of points to plot between the start and end points
+    """
+    # Build up X axis by gathering relatively evely spaced points
+    query_points = [starting_point]
+    query_points += [1 + step*index for index in range(num_of_points)]
+    query_points += [ending_point]
+
     return {
         # Get the head word of each file that will be parsed
         os.path.splitext(os.path.basename(file))[0].split("_")[0]:
         pd.read_csv(file, sep="\t")
         .assign(num_lfs=lambda x: x['num_lfs'].map(lambda y: y if y != 'baseline' else 'BL'))
-        .query("num_lfs in [0,1,6,11,16,20,28,22,30] or num_lfs in [0,1,33,65,97,100]")
+        .query("num_lfs in @query_points", engine="python", local_dict={"query_points":query_points})
         for file in glob.glob(f"{result_dir}/{file_path}")
     }
 
 
-def plot_performance_graph(metric='AUROC', evaluation_set='dev', title="", file_name="", data=None, color_map=None):
+def plot_performance_graph(
+    metric='AUROC', 
+    evaluation_set='dev', 
+    title="", 
+    file_name="", 
+    data=None,
+    color_map=None
+):
+    """
+    Plot the graphs onto a multi-subplot grid using seaborn
+    Args:
+        metric - the metric to plot for the y axis
+        evaluation_set - whehter to plot the dev set or test set
+        title - the main title of the large graph
+        file_name - the name of the file to save the graph
+        data - the dataframe tree to plot the large graph
+        color_map - the color coded to plot each point on
+    """
     fig, axes = plt.subplots(len(file_tree), len(file_tree["DaG"]), figsize=(25, 15), sharey='row')
 
     for row_ind, col in enumerate(data):
@@ -29,12 +69,12 @@ def plot_performance_graph(metric='AUROC', evaluation_set='dev', title="", file_
             if len(data[col][row]) == 0:
                 lower, upper = axes[row_ind][col_ind].get_ylim()
                 axes[row_ind][col_ind].annotate("Coming Soon!!", (0.2, (lower+upper)/2), color="red", fontsize=20)
-                
+
             else:
                 sns.pointplot(
-                    x="num_lfs", y=metric, 
-                    data=data[col][row][evaluation_set], 
-                    ax=axes[row_ind][col_ind], 
+                    x="num_lfs", y=metric,
+                    data=data[col][row][evaluation_set],
+                    ax=axes[row_ind][col_ind],
                     hue="label", ci="sd", scale=1.2,
                     markers=["^", "o"]
                 )
@@ -47,7 +87,7 @@ def plot_performance_graph(metric='AUROC', evaluation_set='dev', title="", file_
                 # if order of error bars
                 # change then this code will not work
                 for idx, item in enumerate(axes[row_ind][col_ind].get_children()):
-                    # if the points in graph 
+                    # if the points in graph
                     # change color map accordingly
                     if idx == 0 or idx==1:
                         item.set_edgecolor([
@@ -71,7 +111,7 @@ def plot_performance_graph(metric='AUROC', evaluation_set='dev', title="", file_
                             item.set_alpha(0.25)
                         else:
                             item.set_color(color_map[row])
-                
+
             # only set first column and first row titles
             if row_ind == 0:
                 axes[row_ind][col_ind].set_title(row, color=color_map[row])
@@ -93,10 +133,10 @@ def plot_performance_graph(metric='AUROC', evaluation_set='dev', title="", file_
         leg = axes.flatten()[3].get_legend()
         leg.legendHandles[0].set_edgecolor('black')
         leg.legendHandles[0].set_facecolor('white')
-        
+
         leg.legendHandles[1].set_edgecolor('black')
         leg.legendHandles[1].set_facecolor('white')
-    
+
     fig.text(0.5, 0.89, 'Label Sources', ha='center', fontsize=30)
     fig.text(0.5, 0.04, 'Number of Additional Label Functions', ha='center', fontsize=30)
     fig.text(0.04, 0.5, f'Predicted Relations ({metric})', va='center', rotation='vertical', fontsize=25)
@@ -105,83 +145,100 @@ def plot_performance_graph(metric='AUROC', evaluation_set='dev', title="", file_
     plt.subplots_adjust(top=0.85)
     plt.savefig(file_name, format='png')
 
+# If running the script itself
+# execute the code below
+if __name__ == '__main__':
 
-color_map = {
-    "DaG": pd.np.array([27, 158, 119])/255,
-    "CtD": pd.np.array([217, 95, 2])/255,
-    "CbG": pd.np.array([117, 112, 179])/255,
-    "GiG": pd.np.array([231, 41, 138])/255,
-    "All": pd.np.array([102, 166, 30])/255
-}
+    file_tree = OrderedDict({
+        "DaG":
+        {
+            "DaG": "../../../disease_gene/disease_associates_gene/label_sampling_experiment/results/DaG/results",
+            "CtD": "../../../disease_gene/disease_associates_gene/label_sampling_experiment/results/CtD/results",
+            "CbG": "../../../disease_gene/disease_associates_gene/label_sampling_experiment/results/CbG/results",
+            "GiG": "../../../disease_gene/disease_associates_gene/label_sampling_experiment/results/GiG/results",
+            "All": "../../../disease_gene/disease_associates_gene/label_sampling_experiment/results/all",
+        },
+        "CtD":
+        {
+            "DaG": "../../../compound_disease/compound_treats_disease/label_sampling_experiment/results/DaG/results",
+            "CtD": "../../../compound_disease/compound_treats_disease/label_sampling_experiment/results/CtD/results",
+            "CbG": "../../../compound_disease/compound_treats_disease/label_sampling_experiment/results/CbG/results",
+            "GiG": "../../../compound_disease/compound_treats_disease/label_sampling_experiment/results/GiG/results",
+            "All": "../../../compound_disease/compound_treats_disease/label_sampling_experiment/results/all",
+        },
 
-file_tree = OrderedDict({
-    "DaG":
-    {
-        "DaG": "../../../disease_gene/disease_associates_gene/label_sampling_experiment/results/DaG/results",
-        "CtD": "../../../disease_gene/disease_associates_gene/label_sampling_experiment/results/CtD/results",
-        "CbG": "../../../disease_gene/disease_associates_gene/label_sampling_experiment/results/CbG/results",
-        "GiG": "../../../disease_gene/disease_associates_gene/label_sampling_experiment/results/GiG/results",
-        "All": "../../../disease_gene/disease_associates_gene/label_sampling_experiment/results/all",
-    },
-    "CtD":
-    {
-        "DaG": "../../../compound_disease/compound_treats_disease/label_sampling_experiment/results/DaG/results",
-        "CtD": "../../../compound_disease/compound_treats_disease/label_sampling_experiment/results/CtD/results",
-        "CbG": "../../../compound_disease/compound_treats_disease/label_sampling_experiment/results/CbG/results",
-        "GiG": "../../../compound_disease/compound_treats_disease/label_sampling_experiment/results/GiG/results",
-        "All": "../../../compound_disease/compound_treats_disease/label_sampling_experiment/results/all",
-    },
+        "CbG":
+        {
+            "DaG": "../../../compound_gene/compound_binds_gene/label_sampling_experiment/results/DaG/results",
+            "CtD": "../../../compound_gene/compound_binds_gene/label_sampling_experiment/results/CtD/results",
+            "CbG": "../../../compound_gene/compound_binds_gene/label_sampling_experiment/results/CbG/results",
+            "GiG": "../../../compound_gene/compound_binds_gene/label_sampling_experiment/results/GiG/results",
+            "All": "../../../compound_gene/compound_binds_gene/label_sampling_experiment/results/all",
+        },
+        "GiG":
+        {
+            "DaG": "../../../gene_gene/gene_interacts_gene/label_sampling_experiment/results/DaG/results",
+            "CtD": "../../../gene_gene/gene_interacts_gene/label_sampling_experiment/results/CtD/results",
+            "CbG": "../../../gene_gene/gene_interacts_gene/label_sampling_experiment/results/CbG/results",
+            "GiG": "../../../gene_gene/gene_interacts_gene/label_sampling_experiment/results/GiG/results",
+            "All": "../../../gene_gene/gene_interacts_gene/label_sampling_experiment/results/all",
+        }
+    })
 
-    "CbG":
-    {
-        "DaG": "../../../compound_gene/compound_binds_gene/label_sampling_experiment/results/DaG/results",
-        "CtD": "../../../compound_gene/compound_binds_gene/label_sampling_experiment/results/CtD/results",
-        "CbG": "../../../compound_gene/compound_binds_gene/label_sampling_experiment/results/CbG/results",
-        "GiG": "../../../compound_gene/compound_binds_gene/label_sampling_experiment/results/GiG/results",
-        "All": "../../../compound_gene/compound_binds_gene/label_sampling_experiment/results/all",
-    },
-    "GiG":
-    {
-        "DaG": "../../../gene_gene/gene_interacts_gene/label_sampling_experiment/results/DaG/results",
-        "CtD": "../../../gene_gene/gene_interacts_gene/label_sampling_experiment/results/CtD/results",
-        "CbG": "../../../gene_gene/gene_interacts_gene/label_sampling_experiment/results/CbG/results",
-        "GiG": "../../../gene_gene/gene_interacts_gene/label_sampling_experiment/results/GiG/results",
-        "All": "../../../gene_gene/gene_interacts_gene/label_sampling_experiment/results/all",
+    # End total of label functions for each point
+    end_points = {
+        "DaG": 30,
+        "CtD": 22,
+        "CbG": 20,
+        "GiG": 28,
+        "All": 100
     }
-})
 
-disc_performance_tree = OrderedDict({
-    key: {
-        sub_key: get_dataframes(file_tree[key][sub_key], "*disc_performance.tsv")
-        for sub_key in file_tree[key]
+    disc_performance_tree = OrderedDict({
+        key: {
+            sub_key: get_dataframes(
+                file_tree[key][sub_key], "*disc_performance.tsv",
+                ending_point=end_points[sub_key],
+                # if using all the label functions step by 32 instead of 5
+                step=5 if sub_key != "All" else 32
+            )
+            for sub_key in file_tree[key]
+        }
+        for key in file_tree
+    })
+
+    color_map = {
+        "DaG": pd.np.array([27, 158, 119])/255,
+        "CtD": pd.np.array([217, 95, 2])/255,
+        "CbG": pd.np.array([117, 112, 179])/255,
+        "GiG": pd.np.array([231, 41, 138])/255,
+        "All": pd.np.array([102, 166, 30])/255
     }
-    for key in file_tree
-})
 
-plot_performance_graph(
-    metric="AUROC", evaluation_set='dev',
-    title="Label Sampling Discriminator Model Assessment (Dev Set)",
-    file_name="../disc_performance_dev_set_auroc.png", data=disc_performance_tree,
-    color_map=color_map
-)
+    plot_performance_graph(
+        metric="AUROC", evaluation_set='dev',
+        title="Label Sampling Discriminator Model Assessment (Dev Set)",
+        file_name="../disc_performance_dev_set_auroc.png", data=disc_performance_tree,
+        color_map=color_map
+    )
 
-plot_performance_graph(
-    metric="AUPRC", evaluation_set='dev',
-    title="Label Sampling Discriminator Model Assessment (Dev Set)",
-    file_name="../disc_performance_dev_set_auprc.png", data=disc_performance_tree,
-    color_map=color_map
-)
+    plot_performance_graph(
+        metric="AUPRC", evaluation_set='dev',
+        title="Label Sampling Discriminator Model Assessment (Dev Set)",
+        file_name="../disc_performance_dev_set_auprc.png", data=disc_performance_tree,
+        color_map=color_map
+    )
 
-plot_performance_graph(
-    metric="AUROC", evaluation_set='test',
-    title="Label Sampling Discriminator Model Assessment (Test Set)",
-    file_name="../disc_performance_test_set_auroc.png", data=disc_performance_tree,
-    color_map=color_map
-)
+    plot_performance_graph(
+        metric="AUROC", evaluation_set='test',
+        title="Label Sampling Discriminator Model Assessment (Test Set)",
+        file_name="../disc_performance_test_set_auroc.png", data=disc_performance_tree,
+        color_map=color_map
+    )
 
-plot_performance_graph(
-    metric="AUPRC", evaluation_set='test',
-    title="Label Sampling Discriminator Model Assessment (Test Set)",
-    file_name="../disc_performance_test_set_auprc.png", data=disc_performance_tree,
-    color_map=color_map
-)
+    plot_performance_graph(
+        metric="AUPRC", evaluation_set='test',
+        title="Label Sampling Discriminator Model Assessment (Test Set)",
+        file_name="../disc_performance_test_set_auprc.png", data=disc_performance_tree,
+        color_map=color_map
+    )
