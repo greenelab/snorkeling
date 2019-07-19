56c39861992926482d1f9e36c15bc32ac41025a7
David Nicholson <dnicholson329@gmail.com>
2019-07-19 11:46:32 -0400

    Fixing the too large file error

---
+++ compound_gene/compound_binds_gene/edge_prediction_experiment/edge_prediction_experiment.py
@@ -0,0 +1,324 @@
+
+# coding: utf-8
+
+# # Compound Binds Gene Edge Prediction
+
+# This notebook is designed to take the next step moving from predicted sentences to edge predictions
+
+# In[1]:
+
+
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import pandas as pd
+from sklearn.metrics import precision_recall_curve, roc_curve, auc
+import seaborn as sns
+import matplotlib.pyplot as plt
+from tqdm import tqdm_notebook
+
+
+# In[2]:
+
+
+total_candidates_df = (
+    pd
+    #.read_table("../dataset_statistics/results/all_cg_candidates.tsv.xz")
+    .read_table("../dataset_statistics/data/all_cbg_candidates.tsv.xz")
+    .sort_values("candidate_id")
+)
+total_candidates_df.head(2)
+
+
+# In[3]:
+
+
+sentence_prediction_df = (
+    pd
+    .read_table("results/all_predicted_sentences.tsv")
+    .sort_values("candidate_id")
+)
+sentence_prediction_df.head(2)
+
+
+# In[4]:
+
+
+total_candidates_pred_df = (
+    total_candidates_df[[
+    "drugbank_id", "name", 
+    "entrez_gene_id", "gene_symbol", 
+    "text", "hetionet",
+    "candidate_id"
+    ]]
+    .rename(index=str, columns={"name": "drug_name"})
+    .merge(sentence_prediction_df, on="candidate_id")
+)
+total_candidates_pred_df.head(2)
+
+
+# In[5]:
+
+
+grouped_candidates_pred_df=(
+    total_candidates_pred_df
+    .groupby(["drugbank_id", "entrez_gene_id"], as_index=False)
+    .agg({
+        "model_prediction": ['max', 'mean', 'median'], 
+        'hetionet': 'max',
+        "gene_symbol": 'first',
+        "drug_name": 'first'
+    })
+)
+grouped_candidates_pred_df.head(2)
+
+
+# In[6]:
+
+
+grouped_candidates_pred_df.columns = [
+    "_".join(col) 
+    if col[1] != '' and col[0] not in ['hetionet', 'gene_symbol', 'drug_name'] else col[0] 
+    for col in grouped_candidates_pred_df.columns.values
+]
+grouped_candidates_pred_df.head(2)
+
+
+# In[7]:
+
+
+grouped_candidates_pred_df.hetionet.value_counts()
+
+
+# In[8]:
+
+
+performance_map = {}
+
+
+# In[9]:
+
+
+precision, recall, pr_threshold = precision_recall_curve(
+    grouped_candidates_pred_df.hetionet, 
+    grouped_candidates_pred_df.model_prediction_max,
+)
+
+fpr, tpr, roc_threshold = roc_curve(
+    grouped_candidates_pred_df.hetionet, 
+    grouped_candidates_pred_df.model_prediction_max,
+)
+
+performance_map['max'] = {
+    "precision":precision, "recall":recall, 
+    "pr_threshold":pr_threshold, "false_pos":fpr,
+    "true_pos":tpr, "roc_threshold":roc_threshold, 
+}
+
+
+# In[10]:
+
+
+precision, recall, pr_threshold = precision_recall_curve(
+    grouped_candidates_pred_df.hetionet, 
+    grouped_candidates_pred_df.model_prediction_mean,
+)
+
+fpr, tpr, roc_threshold = roc_curve(
+    grouped_candidates_pred_df.hetionet, 
+    grouped_candidates_pred_df.model_prediction_mean,
+)
+
+performance_map['mean'] = {
+    "precision":precision, "recall":recall, 
+    "pr_threshold":pr_threshold, "false_pos":fpr,
+    "true_pos":tpr, "roc_threshold":roc_threshold, 
+}
+
+
+# In[11]:
+
+
+precision, recall, pr_threshold = precision_recall_curve(
+    grouped_candidates_pred_df.hetionet, 
+    grouped_candidates_pred_df.model_prediction_median,
+)
+
+fpr, tpr, roc_threshold = roc_curve(
+    grouped_candidates_pred_df.hetionet, 
+    grouped_candidates_pred_df.model_prediction_median,
+)
+
+performance_map['median'] = {
+    "precision":precision, "recall":recall, 
+    "pr_threshold":pr_threshold, "false_pos":fpr,
+    "true_pos":tpr, "roc_threshold":roc_threshold, 
+}
+
+
+# In[12]:
+
+
+for key in performance_map:
+    plt.plot(
+        performance_map[key]['false_pos'], 
+        performance_map[key]['true_pos'], 
+        label=f"{key}:AUC ({auc(performance_map[key]['false_pos'], performance_map[key]['true_pos']):.3f})"
+    )
+plt.plot([0,1], [0,1], linestyle='--', color='black')
+plt.legend()
+plt.show()
+
+
+# In[13]:
+
+
+# https://stackoverflow.com/questions/28719067/roc-curve-and-cut-off-point-python
+def Find_Optimal_Cutoff(target, predicted):
+    """ Find the optimal probability cutoff point for a classification model related to event rate
+    Parameters
+    ----------
+    target : Matrix with dependent or target data, where rows are observations
+
+    predicted : Matrix with predicted data, where rows are observations
+
+    Returns
+    -------     
+    list type, with optimal cutoff value
+
+    """
+    fpr, tpr, threshold = roc_curve(target, predicted)
+    i = pd.np.arange(len(tpr)) 
+    roc = pd.DataFrame({'tf' : pd.Series(tpr-(1-fpr), index=i), 'threshold' : pd.Series(threshold, index=i)})
+    roc_t = roc.ix[(roc.tf-0).abs().argsort()[:1]]
+
+    return list(roc_t['threshold']) 
+roc_optimal = Find_Optimal_Cutoff(grouped_candidates_pred_df.hetionet.values, grouped_candidates_pred_df.model_prediction_max)[0]
+roc_optimal
+
+
+# In[14]:
+
+
+for key in performance_map:
+    plt.plot(
+        performance_map[key]['recall'], 
+        performance_map[key]['precision'], 
+        label=f"{key}:AUC ({auc(performance_map[key]['recall'], performance_map[key]['precision']):.3f})"
+    )
+
+plt.legend()
+plt.show()
+
+
+# # Optimal PR-Cutoff
+
+# In[15]:
+
+
+threshold_df = (
+    pd.DataFrame(
+        list(
+            zip(
+                performance_map['max']['precision'], 
+                performance_map['max']['recall'], 
+                performance_map['max']['pr_threshold']
+            )
+        ),
+        columns=["precision", "recall", "pr_threshold"]
+    )
+    .sort_values("precision", ascending=False)
+)
+threshold_df.head(2)
+
+
+# In[16]:
+
+
+#precision_thresholds = pd.np.linspace(0,1,num=5)
+precision_thresholds = threshold_df.round(2).drop_duplicates("precision").precision.values
+
+# Add the lowest precision rather than
+# Keep it zero
+precision_thresholds = (
+    pd.np.where(
+        precision_thresholds==0, 
+        threshold_df.query("precision > 0").precision.min(), 
+        precision_thresholds
+    )
+)
+
+performance_records = []
+for precision_cutoff in tqdm_notebook(precision_thresholds):
+
+    cutoff = (
+        threshold_df
+        .query("precision>=@precision_cutoff")
+        .pr_threshold
+        .min()
+    )
+    
+    values_added = (
+        grouped_candidates_pred_df
+        .query("model_prediction_max >= @cutoff")
+        .hetionet
+        .value_counts()
+    )
+    
+    series_keys = list(values_added.keys())
+    for key in series_keys:
+        performance_records.append(
+           {  
+               "edges": values_added[key], 
+               "in_hetionet": "Existing" if key == 1 else "Novel", 
+               "precision": precision_cutoff,
+               "sen_cutoff": cutoff
+           }
+        )
+   
+    
+edges_added_df = (
+    pd
+    .DataFrame
+    .from_records(performance_records)
+)
+edges_added_df.head(10)
+
+
+# In[17]:
+
+
+ax = sns.scatterplot(x="precision", y="edges", hue="in_hetionet", data=edges_added_df)
+ax.set(yscale="log")
+
+
+# In[18]:
+
+
+edges_added_df.to_csv("results/precision_cbg_edges_added.tsv", index=False, sep="\t")
+
+
+# # Optimal ROC Cutoff
+
+# In[19]:
+
+
+(
+    grouped_candidates_pred_df
+    .query("model_prediction_max > @roc_optimal")
+    .sort_values("hetionet")
+    .hetionet.value_counts()
+)
+
+
+# In[20]:
+
+
+(
+    grouped_candidates_pred_df
+    .query("model_prediction_max > @roc_optimal")
+    .sort_values(["hetionet", "model_prediction_max"], ascending=[True, False])
+    .head(10)
+)
+
---
+++ figures/edge_prediction_experiment/plot_scripts/edge_prediction.py
@@ -0,0 +1,184 @@
+import os
+import glob
+from collections import OrderedDict
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pdb
+import re
+
+plt.switch_backend('agg')
+
+
+def get_dataframes(
+    result_dir, file_path,
+    starting_point=0,
+    ending_point=30,
+    step=5, num_of_points=4
+):
+    """
+     This function grabs the result tsv files
+     and loads then into a dictionary strucutre
+     [relationship] -> dataframe
+
+     Args:
+         result_dir - the directory containing all the results
+         file_path - the path to extract the result files
+         starting_point - the point to start each subgraph with in plot_graph function
+         ending_point - the point to end each subgraph with
+         step - the number to increase the middle points with
+         num_of_points - the number of points to plot between the start and end points
+    """
+    query_points = [0.25 * i for i in range(5)]
+
+    return {
+        # Get the head word of each file that will be parsed
+        re.search("(precision)", file).group(0):
+        (
+            pd.read_csv(file, sep="\t")
+            .round({"precision": 2})
+            .drop_duplicates(["precision", "in_hetionet"])
+            .sort_values("precision", ascending=False)
+        )
+        for file in glob.glob(f"{result_dir}{file_path}")
+    }
+
+
+def plot_performance_graph(
+    metric='AUROC',
+    evaluation_set='dev',
+    title="",
+    file_name="",
+    data=None,
+    color_map=None
+):
+    """
+    Plot the graphs onto a multi-subplot grid using seaborn
+    Args:
+        metric - the metric to plot for the y axis
+        evaluation_set - whehter to plot the dev set or test set
+        title - the main title of the large graph
+        file_name - the name of the file to save the graph
+        data - the dataframe tree to plot the large graph
+        color_map - the color coded to plot each point on
+    """
+    fig, axes = plt.subplots(len(file_tree["DaG"]), len(file_tree), figsize=(22, 10), sharey='row', sharex='row')
+
+    for row_ind, col in enumerate(data):
+        for row in data[col]:
+
+            if "precision" not in data[col][row]:
+                continue
+            data[col][row][evaluation_set] = (
+                data[col][row][evaluation_set]
+                .groupby([
+                    pd.cut(
+                        data[col][row][evaluation_set]["precision"],
+                        pd.np.arange(0, 1, 0.1)
+                    ),
+                    "in_hetionet"
+                ])
+                .max()
+                .reset_index(level="in_hetionet")
+                .reset_index(drop=True)
+                .append(data[col][row][evaluation_set].query("precision==1"), sort=True)
+            )
+
+            # plot the graph
+            sns.scatterplot(
+                x="precision", y=metric,
+                hue="in_hetionet",
+                data=data[col][row][evaluation_set].sort_values("in_hetionet"),
+                ax=axes[row_ind], ci=None,
+            )
+
+            # print(data[col][row][evaluation_set])
+
+            # remove x axis labels
+            axes[row_ind].set_xlabel('')
+
+            axes[row_ind].set_title(row, color=color_map[row])
+            axes[row_ind].set(yscale="log")
+            axes[row_ind].set_ylim([10**(-1/8), 10e6])
+            axes[row_ind].set_xlim([0, 1.05])
+            axes[row_ind].set_ylabel("")
+            axes[row_ind].get_legend().remove()
+
+    # Change the font for each element of the graph
+    for item in axes.flat:
+        item.title.set_fontsize(20)
+        item.yaxis.label.set_fontsize(15)
+        item.xaxis.label.set_fontsize(15)
+        for tick in item.get_yticklabels() + item.get_xticklabels():
+            tick.set_fontsize(15)
+
+    axes.flatten()[2].legend(loc='upper center', bbox_to_anchor=(2.6, 1.0), fontsize=15)
+    # Add the subtitles and save the graph
+    #fig.text(0.5, 0.89, '', ha='center', fontsize=26)
+    fig.text(0.5, 0.02, 'Precision Level', ha='center', fontsize=20)
+    fig.text(0.04, 0.5, f'Number of Edges', va='center', rotation='vertical', fontsize=20)
+    fig.suptitle(title, fontsize=25)
+    plt.subplots_adjust(top=0.85, right=0.85)
+    plt.savefig(file_name, format='png')
+
+# If running the script itself
+# execute the code below
+if __name__ == '__main__':
+    file_tree = OrderedDict({
+        "DaG":
+        {
+            "DaG": "../../../disease_gene/disease_associates_gene/edge_prediction_experiment/results/"
+        },
+        "CtD":
+        {
+            "CtD": "../../../compound_disease/compound_treats_disease/edge_prediction_experiment/results/"
+        },
+
+        "CbG":
+        {
+            "CbG": "../../../compound_gene/compound_binds_gene/edge_prediction_experiment/results/"
+
+        },
+        "GiG":
+        {
+            "GiG": "../../../gene_gene/gene_interacts_gene/edge_prediction_experiment/results/"
+
+        }
+    })
+
+
+    # Use the file tree above and graph the appropiate files
+    edges_data_tree = OrderedDict({
+        key: {
+            sub_key: get_dataframes(
+                file_tree[key][sub_key], "*edges_added.tsv",
+            )
+            for sub_key in file_tree[key]
+        }
+        for key in file_tree
+    })
+
+    # Obtained from color brewer 2
+    # 5 classes using the supposedly color blind friendly colors
+    color_names = {
+        "turquoise": pd.np.array([27, 158, 119])/255,
+        "orange": pd.np.array([217, 95, 2])/255,
+        "purple": pd.np.array([117, 112, 179])/255,
+        "pink": pd.np.array([231, 41, 138])/255,
+        "light-green": pd.np.array([102, 166, 30])/255
+    }
+
+    color_map = {
+        "DaG": color_names["turquoise"],
+        "CtD": color_names["orange"],
+        "CbG": color_names["purple"],
+        "GiG": color_names["pink"],
+        "All": color_names["light-green"]
+    }
+
+    plot_performance_graph(
+        metric="edges", evaluation_set='precision',
+        title="Reconstructing Hetionet",
+        file_name="../edges_added.png", data=edges_data_tree,
+        color_map=color_map
+    )
